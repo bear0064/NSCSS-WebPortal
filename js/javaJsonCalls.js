22b61a78e36793d69aebafe7076279e80710082b
bear6310 <bear0064@algonquinlive.com>
2015-03-10 15:47:45 -0400

    Javascript/Icons/Menus
    Fixed Menu’s, added Icons

---
+++ js/javaJsonCalls.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+var algomaMills;
+var blindRiver;
+var elliotLake;
+var ironBridge;
+var mississauga;
+var serpentRiver;
+var serpentRiverFN;
+var spragge;
+var spanish;
+var thessalon;
+var thessalonFN;
+var alldat;
+    
+    
+    $("#algomaMills").click(function () {
+    
+        
+    
+    })
+                           
+}); // end ready
