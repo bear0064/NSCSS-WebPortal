a27adf9e06821a037894f252fd45f55028f29ef6
bear6310 <bear0064@algonquinlive.com>
2015-03-21 23:53:07 -0400

    Fixed Header, added quick appends to show data

--- js/javaJsonCalls.js
+++ js/javaJsonCalls.js
@@ -1,76 +1,90 @@
-var communityId;    
+var communityId;
 var categoryId;
-var qry; 
+var qry;
 var data;
 
 $(document).ready(function () {
 
-  $("#location-nav").click(function (event) {
-      event.preventDefault();
+    $("#location-nav").click(function (event) {
+        event.preventDefault();
         $(".menu").slideToggle();
-      
-     //Set communityId for side menu link build     
-});
 
-       
-    
-$(".community").click(function (event) {
-            event.preventDefault();
-
-            
-$("#main-nav").show(); 
-$("#main").animate({"left":"250px"}, "slow"); 
-
-            
-  var originalText = $(this).text(); 
-  var modifiedText = originalText.replace("original", "cloned"); 
-  $(".section-title>p").text(modifiedText);
-            
-
-            $(".col-1").empty()
+        //Set communityId for side menu link build     
+    });
 
 
 
-        });
+    $(".community").click(function (event) {
+        event.preventDefault();
+
+        communityId = $(this).attr("data-community");
+
         
-    
-  
-$(".sdcommunity").click(function (event) {
-            event.preventDefault();
+        $("#main-nav").show();
+        $("#main").animate({
+            "left": "250px"
+        }, "slow");
 
-communityId = $(this).attr("data-community");
-            
-$("#main-nav").show(); 
-$("#main").animate({"left":"250px"}, "slow"); 
 
-            
-  var originalText = $(this).text(); 
-  var modifiedText = originalText.replace("original", "cloned"); 
-  $(".section-title>p").text(modifiedText);
-            
+        var originalText = $(this).text();
+        var modifiedText = originalText.replace("original", "cloned");
+        $(".section-title>p").text(modifiedText);
 
-            $(".col-1").empty()
-$(".menu").slideToggle();
-            
-        });
-    
-    
-    
-    
-    
-    
-    
-    
-$(".service").click(function(){
-        
-     categoryId = $(this).attr("data-category");
-     //Set categoryId for query out
+
+        $(".col-1").empty()
+
+
+
+    });
+
+
+
+    $(".sdcommunity").click(function (event) {
+        event.preventDefault();
+
+        communityId = $(this).attr("data-community");
+
+        $("#main-nav").show();
+        $("#main").animate({
+            "left": "250px"
+        }, "slow");
+
+
+        var originalText = $(this).text();
+        var modifiedText = originalText.replace("original", "cloned");
+        $(".section-title>p").text(modifiedText);
+
+
+        $(".col-1").empty()
+        $(".menu").slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+    $(".service").click(function () {
+
+        categoryId = $(this).attr("data-category");
+        //Set categoryId for query out
         console.log(this);
-    //link to a method to create query and send for the data    
-    sendQry();        
+        //link to a method to create query and send for the data    
+
+        var originalText = $(this).text();
+        var modifiedText = originalText.replace("original", "cloned");
+        $(".serviceHeader").text(modifiedText);
+
+
+        //$(".col-1").empty()
         
-    
-});
+        sendQry();
+
+
+    });
 
 });
 // end ready
@@ -79,53 +93,59 @@ $(".service").click(function(){
 
 
 
-function sendQry(){
-    
+function sendQry() {
+
     //prep ajax call and qry to server
-  
-
-
-console.log(communityId);
-console.log(categoryId);
-  
-
-qry = "http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=communities+asc&categories=%3D" + categoryId + "&communities=%3D" + communityId + "&-action=export_json";
-    
-
-qry1 ='http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=communities+asc&categories=%3D13&communities=%3D1&-action=export_json';
 
 
 
-var request = $.ajax({
-  url: qry,
-  type: "GET",
-  dataType: "json"
-});
+    console.log(communityId);
+    console.log(categoryId);
 
-request.done(function(msg) {
-  console.log(msg)
-  data = msg;
-    
-    displayRes();
-    
-});
 
-request.fail(function(jqXHR, textStatus) {
-  alert( "Request failed: " + textStatus );
-});
+    qry = "http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=name_en+asc&categories=%3D" + categoryId + "&communities=%3D" + communityId + "&-action=export_json";
+
+
+    qry1 = 'http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=communities+asc&categories=%3D13&communities=%3D1&-action=export_json';
+
+
+
+    var request = $.ajax({
+        url: qry,
+        type: "GET",
+        dataType: "json"
+    });
+
+    request.done(function (msg) {
+        console.log(msg)
+        data = msg;
+
+        displayRes();
+
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
 
 }
 
 
-function displayRes(){
+function displayRes() {
     
-    
-for (i=0; i < data.length; i++){
-    console.log(data[i].name_en)
-    }    
-    
-    
-}
+    $(".loaded").empty();
 
-  
-  
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].name_en)
+        
+        var s = "";
+        
+        s += "<div class='listData'>";
+		s += "<p>"+data[i].name_en+"</hp>";
+        s += "</div>";        
+		document.querySelector(".loaded").innerHTML += s;
+        
+        
+
+    }
+}
