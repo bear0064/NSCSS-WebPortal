0e539d149e5f50f8f9f90d5e79e2282cf255c0ee
bear6310 <bear0064@algonquinlive.com>
2015-03-21 22:48:05 -0400

    Menus,Ajax,Headers
    Fixed naming conventions, added menu support and hacked me some Ajax to
    work with model data.

--- js/javaJsonCalls.js
+++ js/javaJsonCalls.js
@@ -1,23 +1,131 @@
+var communityId;    
+var categoryId;
+var qry; 
+var data;
+
 $(document).ready(function () {
 
-var algomaMills;
-var blindRiver;
-var elliotLake;
-var ironBridge;
-var mississauga;
-var serpentRiver;
-var serpentRiverFN;
-var spragge;
-var spanish;
-var thessalon;
-var thessalonFN;
-var alldat;
+  $("#location-nav").click(function (event) {
+      event.preventDefault();
+        $(".menu").slideToggle();
+      
+     //Set communityId for side menu link build     
+});
+
+       
     
-    
-    $("#algomaMills").click(function () {
-    
+$(".community").click(function (event) {
+            event.preventDefault();
+
+            
+$("#main-nav").show(); 
+$("#main").animate({"left":"250px"}, "slow"); 
+
+            
+  var originalText = $(this).text(); 
+  var modifiedText = originalText.replace("original", "cloned"); 
+  $(".section-title>p").text(modifiedText);
+            
+
+            $(".col-1").empty()
+
+
+
+        });
         
     
-    })
-                           
-}); // end ready
+  
+$(".sdcommunity").click(function (event) {
+            event.preventDefault();
+
+communityId = $(this).attr("data-community");
+            
+$("#main-nav").show(); 
+$("#main").animate({"left":"250px"}, "slow"); 
+
+            
+  var originalText = $(this).text(); 
+  var modifiedText = originalText.replace("original", "cloned"); 
+  $(".section-title>p").text(modifiedText);
+            
+
+            $(".col-1").empty()
+$(".menu").slideToggle();
+            
+        });
+    
+    
+    
+    
+    
+    
+    
+    
+$(".service").click(function(){
+        
+     categoryId = $(this).attr("data-category");
+     //Set categoryId for query out
+        console.log(this);
+    //link to a method to create query and send for the data    
+    sendQry();        
+        
+    
+});
+
+});
+// end ready
+
+
+
+
+
+function sendQry(){
+    
+    //prep ajax call and qry to server
+  
+
+
+console.log(communityId);
+console.log(categoryId);
+  
+
+qry = "http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=communities+asc&categories=%3D" + categoryId + "&communities=%3D" + communityId + "&-action=export_json";
+    
+
+qry1 ='http://www.nscss.com/EAConnects/admin/index.php?-table=programs&-sort=communities+asc&categories=%3D13&communities=%3D1&-action=export_json';
+
+
+
+var request = $.ajax({
+  url: qry,
+  type: "GET",
+  dataType: "json"
+});
+
+request.done(function(msg) {
+  console.log(msg)
+  data = msg;
+    
+    displayRes();
+    
+});
+
+request.fail(function(jqXHR, textStatus) {
+  alert( "Request failed: " + textStatus );
+});
+
+}
+
+
+function displayRes(){
+    
+    
+for (i=0; i < data.length; i++){
+    console.log(data[i].name_en)
+    }    
+    
+    
+}
+
+  
+  
